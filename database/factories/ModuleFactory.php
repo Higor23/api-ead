e9f8f99b081ffe97ebefb81deb7ec1e5b13071d7
higor23 <higor.lemospr@gmail.com>
2022-03-07 21:16:36 -0300

    Create module Lesson

---
+++ database/factories/ModuleFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Module;
+use App\Models\Course;
+
+class ModuleFactory extends Factory
+{
+    
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Module::class;
+    
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'course_id' => Course::factory(),
+            'name' => $this->faker->name()
+        ];
+    }
+}
